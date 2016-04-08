3e25508afb2dff8c8160cf6d1899a83b7e0d5d89
Falko Bräutigam <falko@polymap.de>
2016-04-08 23:11:22 +0200

    #29: Arena jenkins build (http://github.com/Mapzone/mapzone/issues/issue/29)

--- io.mapzone.arena/src/io/mapzone/arena/analytics/chart/ChartPanel.java
+++ io.mapzone.arena/src/io/mapzone/arena/analytics/chart/ChartPanel.java
@@ -257,8 +257,8 @@ public class ChartPanel
                 ProcessorDescription proc = new ProcessorDescription( ChartGeometryProcessor.class, props );
                 PipelineProcessorSite procSite = new PipelineProcessorSite( props );
                 proc.processor().init( procSite );
-                featureRenderProc.pipeline.get().add( 0, proc );
-                log.info( "FeatureRender pipeline: " + featureRenderProc.pipeline.get() );
+                featureRenderProc.pipeline().add( 0, proc );
+                log.info( "FeatureRender pipeline: " + featureRenderProc.pipeline() );
                 
                 // register WMS servlet
                 servletAlias = "/chart" + hashCode();
