df96390f62bb6cceac4a9659c46c1a5e43cd549b
Steffen Stundzig <git@stundzig.de>
2016-08-19 13:49:34 +0200

    55: [Arena] Share data page
    Task-Url: https://github.com/Mapzone/mapzone/issues/issue/55
    
      - replace shareinfoservlet with download service

--- io.mapzone.arena/src/io/mapzone/arena/share/content/ArenaContentBuilder.java
+++ io.mapzone.arena/src/io/mapzone/arena/share/content/ArenaContentBuilder.java
@@ -23,7 +23,6 @@ import com.vividsolutions.jts.geom.Envelope;
 
 import io.mapzone.arena.ArenaPlugin;
 import io.mapzone.arena.share.ShareInfoServlet;
-import io.mapzone.arena.share.ShareServletsStarter;
 import io.mapzone.arena.share.ui.ShareContext;
 import io.mapzone.arena.share.ui.ShareContext.SelectionDescriptor;
 
@@ -64,10 +63,9 @@ public class ArenaContentBuilder
         extent.add( String.valueOf( (int)envelope.getMaxX() ) );
         extent.add( String.valueOf( (int)envelope.getMaxY() ) );
         
-        StringBuffer shareInfo = new StringBuffer( ArenaPlugin.instance().config().getProxyUrl() );
-        shareInfo.append( ShareServletsStarter.ALIAS_SHAREINFO ).append( "?" );
+        StringBuffer shareInfo = new StringBuffer( );
         try {
-            shareInfo.append( ShareInfoServlet.PARAMETER_LAYERS ).append( "=" ).append( URLEncoder.encode( layers.toString(), "utf-8") );
+            shareInfo.append( "&" ).append( ShareInfoServlet.PARAMETER_LAYERS ).append( "=" ).append( URLEncoder.encode( layers.toString(), "utf-8") );
             shareInfo.append( "&" ).append( ShareInfoServlet.PARAMETER_BBOX ).append( "=" ).append( URLEncoder.encode(extent.toString(), "utf-8" ));
             if (!StringUtils.isBlank( ArenaPlugin.instance().config().getServiceAuthToken() )) {
                 shareInfo.append( "&" ).append( ShareInfoServlet.PARAMETER_AUTHTOKEN ).append( "=" ).append( URLEncoder.encode(ArenaPlugin.instance().config().getServiceAuthToken(), "utf-8" ));
