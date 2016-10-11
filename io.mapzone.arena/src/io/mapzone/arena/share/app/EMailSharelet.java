88fe7d21cea8fc82a28b399ed1d794b31384dd66
Falko Bräutigam <falko@polymap.de>
2016-10-11 23:15:39 +0200

    #88: Sharelet for WMS/WFS (http://github.com/Mapzone/mapzone/issues/issue/88)

--- io.mapzone.arena/src/io/mapzone/arena/share/app/EMailSharelet.java
+++ io.mapzone.arena/src/io/mapzone/arena/share/app/EMailSharelet.java
@@ -33,9 +33,6 @@ import javax.mail.internet.MimeMessage.RecipientType;
 import javax.mail.internet.MimeMultipart;
 
 import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
 import com.google.common.collect.Lists;
 
 import org.eclipse.swt.SWT;
@@ -45,15 +42,12 @@ import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 
 import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
-import org.eclipse.rap.rwt.RWT;
-
 import org.polymap.core.operation.DefaultOperation;
 import org.polymap.core.operation.OperationSupport;
 import org.polymap.core.runtime.i18n.IMessages;
@@ -67,6 +61,7 @@ import io.mapzone.arena.ArenaPlugin;
 import io.mapzone.arena.Messages;
 import io.mapzone.arena.share.content.ArenaContentProvider.ArenaContent;
 import io.mapzone.arena.share.content.ImagePngContentProvider.ImagePngContent;
+import io.mapzone.arena.share.DefaultSectionProvider;
 import io.mapzone.arena.share.Sharelet;
 import io.mapzone.arena.share.ShareletSectionProvider;
 import io.mapzone.arena.share.ShareletSite;
@@ -80,8 +75,6 @@ import io.mapzone.arena.share.content.ShareableContentProvider;
 public class EMailSharelet
         extends Sharelet {
 
-    private static Log             log  = LogFactory.getLog( EMailSharelet.class );
-
     private static final IMessages i18n = Messages.forPrefix( "EMailSharelet" );
 
     private ContentProvider        provider;
@@ -94,99 +87,101 @@ public class EMailSharelet
         site.title.set( i18n.get( "title" ) );
         site.description.set( i18n.get( "description" ) );
         site.priority.set( 211 );
-        site.image.set( ArenaPlugin.images().svgImage( "email.svg", SvgImageRegistryHelper.NORMAL48 ) );
+        site.image.set( ArenaPlugin.images().svgImage( "email-outline.svg", SvgImageRegistryHelper.NORMAL48 ) );
         super.init( site );
     }
 
 
-    private ShareletSectionProvider mailform() {
-        return new ShareletSectionProvider() {
-
-            @Override
-            public String title() {
-                return i18n.get( "email_title" );
-            }
+    public List<ShareletSectionProvider> sections() {
+        return Lists.newArrayList( new MailFormSectionProvider() );
+    }
 
 
-            @Override
-            public String[] supportedTypes() {
-                return new String[] { "image/png", "application/arena" };
-            }
+    /**
+     * 
+     */
+    class MailFormSectionProvider
+            extends DefaultSectionProvider {
 
+        @Override
+        public String title() {
+            return i18n.get( "email_title" );
+        }
 
-            @Override
-            public void createContents( final Composite parent, ShareableContentProvider... contentBuilders ) {
-                ImagePngContent image = (ImagePngContent)contentBuilders[0].get();
-                ArenaContent arena = (ArenaContent)contentBuilders[1].get();
-                Button fab = tk().createFab();
+        @Override
+        public String[] supportedTypes() {
+            return new String[] { "image/png", "application/arena" };
+        }
 
-                ColumnDataFactory.on( tk().createLabel( parent, i18n.get( "email_to" ), SWT.WRAP ) ).widthHint( preferredWidth( parent ) );
+        @Override
+        public void createContents( final Composite parent, ShareableContentProvider... contentBuilders ) {
+            ImagePngContent image = (ImagePngContent)contentBuilders[0].get();
+            ArenaContent arena = (ArenaContent)contentBuilders[1].get();
+            Button fab = tk().createFab();
 
-                Text to = tk().createText( parent, "", SWT.BORDER, SWT.WRAP );
-                to.addModifyListener( new ModifyListener() {
-
-                    @Override
-                    public void modifyText( ModifyEvent event ) {
-                        // TODO add check for correct email adresses here
-                        if (!StringUtils.isBlank( to.getText().trim() )) {
-                            fab.setVisible( true );
-                            fab.setEnabled( true );
-                        }
-                        else {
-                            fab.setVisible( false );
-                            fab.setEnabled( false );
-                        }
+            // To:
+            LabeledField toField = createField( tk(), parent, i18n.get( "email_to" ), null ); 
+            Text to = tk().createText( toField, "", SWT.BORDER, SWT.WRAP );
+            to.setToolTipText( i18n.get( "email_toTooltip" ) );
+            ColumnDataFactory.on( to ).widthHint( width() ).heightHint( 40 );
+            to.addModifyListener( new ModifyListener() {
+                @Override
+                public void modifyText( ModifyEvent event ) {
+                    // TODO add check for correct email adresses here
+                    if (!StringUtils.isBlank( to.getText().trim() )) {
+                        fab.setVisible( true );
+                        fab.setEnabled( true );
                     }
-                } );
-                ColumnDataFactory.on( to ).widthHint( preferredWidth( parent ) ).heightHint( 40 );
-
-                ColumnDataFactory.on( tk().createLabel( parent, i18n.get( "email_subject" ), SWT.WRAP ) ).widthHint( preferredWidth( parent ) );
-
-                Text subject = tk().createText( parent, "", SWT.BORDER );
-                ColumnDataFactory.on( subject ).widthHint( preferredWidth( parent ) );
-
-                ColumnDataFactory.on( tk().createLabel( parent, i18n.get( "email_message" ), SWT.WRAP ) ).widthHint( preferredWidth( parent ) );
-
-                Text message = tk().createText( parent, i18n.get( "email_message_text", arena.arena ), SWT.BORDER, SWT.WRAP );
-                ColumnDataFactory.on( message ).widthHint( preferredWidth( parent ) ).heightHint( 120 );
-
-                Button attachPreview = tk().createButton( parent, "", SWT.CHECK );
-                attachPreview.setText( i18n.get( "email_preview" ) );
-                attachPreview.setSelection( true );
-                ColumnDataFactory.on( attachPreview ).widthHint( preferredWidth( parent ) );
-
-                StringBuffer oreview = new StringBuffer( "<img width='" ).append( image.previewWidth ).append( "' height='" ).append( image.previewHeight ).append( "' src='" );
-                oreview.append( image.previewResource );
-                oreview.append( "'/>" );
-
-                Label l = tk().createLabel( parent, oreview.toString(), SWT.BORDER );
-                l.setData( RWT.MARKUP_ENABLED, Boolean.TRUE );
-                ColumnDataFactory.on( l ).widthHint( Math.min( preferredWidth( parent ), image.previewWidth ) ).heightHint( image.previewHeight );
-
-                fab.setVisible( false );
-                fab.addSelectionListener( new SelectionAdapter() {
-
-                    @Override
-                    public void widgetSelected( SelectionEvent e ) {
-                        final String toText = to.getText();
-                        final String subjectText = subject.getText();
-                        final String messageText = message.getText();
-                        final boolean attach = attachPreview.getSelection();
-                        DefaultOperation op = new DefaultOperation( i18n.get( "email_title" ) ) {
-
-                            @Override
-                            public IStatus doExecute( IProgressMonitor monitor, IAdaptable info ) throws Exception {
-                                sendEmail( toText, subjectText, messageText, attach, image );
-                                return Status.OK_STATUS;
-                            }
-                        };
-                        // execute
-                        OperationSupport.instance().execute2( op, true, false );
+                    else {
                         fab.setVisible( false );
+                        fab.setEnabled( false );
                     }
-                } );
-            }
-        };
+                }
+            });                
+
+            // Subject:
+            LabeledField subjectField = createField( tk(), parent, i18n.get( "email_subject" ), null ); 
+            Text subject = adaptLayout( tk().createText( subjectField, "", SWT.BORDER ) );
+
+            // message
+            Text message = tk().createText( parent, i18n.get( "email_message_text", arena.arena ), SWT.BORDER, SWT.WRAP );
+            ColumnDataFactory.on( message ).widthHint( width() ).heightHint( 120 );
+
+            // attachment
+            Button attachPreview = adaptLayout( tk().createButton( parent, i18n.get( "email_preview" ), SWT.CHECK ) );
+            attachPreview.setSelection( true );
+
+            StringBuilder oreview = new StringBuilder( 1024 )
+                    .append( "<img width='" ).append( image.previewWidth )
+                    .append( "' height='" ).append( image.previewHeight )
+                    .append( "' src='" ).append( image.previewResource )
+                    .append( "'/>" );
+
+            ColumnDataFactory.on( tk().createLabel( parent, oreview.toString(), SWT.BORDER ) )
+                    .widthHint( Math.min( preferredWidth( parent ), image.previewWidth ) )
+                    .heightHint( image.previewHeight );
+
+            fab.setVisible( false );
+            fab.addSelectionListener( new SelectionAdapter() {
+                @Override
+                public void widgetSelected( SelectionEvent e ) {
+                    final String toText = to.getText();
+                    final String subjectText = subject.getText();
+                    final String messageText = message.getText();
+                    final boolean attach = attachPreview.getSelection();
+                    DefaultOperation op = new DefaultOperation( i18n.get( "email_title" ) ) {
+                        @Override
+                        public IStatus doExecute( IProgressMonitor monitor, IAdaptable info ) throws Exception {
+                            sendEmail( toText, subjectText, messageText, attach, image );
+                            return Status.OK_STATUS;
+                        }
+                    };
+                    // execute
+                    OperationSupport.instance().execute2( op, true, false );
+                    fab.setVisible( false );
+                }
+            });
+        }
     }
 
 
@@ -262,10 +257,5 @@ public class EMailSharelet
 
     private int preferredWidth( Composite parent ) {
         return Math.min( parent.getDisplay().getClientArea().width, site().preferredWidth.get() ) - 50;
-    }
-
-
-    public List<ShareletSectionProvider> sections() {
-        return Lists.newArrayList( mailform() );
     }
 }
